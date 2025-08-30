677c3a84403e4571191eb72e13be2d6299d46ea6
Andrea Vincenzo <andreabbondanza.developer@outlook.com>
2025-08-30 21:25:05 +0200

    Refactor dependency management and update initialization script
    - Added `requirements-dev.in` for improved dependency specification.
    - Removed `requirements-dev.txt` and `requirements.txt` to streamline the dependency management process.
    - Updated `.devcontainer/init.sh` to install `pip-review` and `pip-tools`, and to use `pip-sync` for syncing dependencies, enhancing the development environment setup.

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -6,5 +6,13 @@ server_port=8000
 
 pip install --upgrade pip
 
+pip install pip-review pip-tools
+
+pip-compile -U --output-file requirements-dev.txt requirements-dev.in
+
 pip install -r requirements-dev.txt
-pip install -r requirements.txt
+
+pip-sync requirements-dev.txt
+
+pip-review --local
+
